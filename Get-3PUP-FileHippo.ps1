606babeb99b2e96c6e3bfcd0481684677382591d
thecatdidit <jayharper@gmail.com>
2018-08-27 10:04:33 -0400

    Update Get-3PUP-FileHippo.ps1

--- Get-3PUP-FileHippo.ps1
+++ Get-3PUP-FileHippo.ps1
@@ -12,9 +12,9 @@
 	This script is designed to query for the current versions of third party products
 	that are supported on a current assignment.
 	The 3PUP versions are checked via FileHippo and emailed at the end of the script.
-        You can configure all aspects of the email (SMTP server, recipients, body, etc.)
-        The body is designed to be an HTML-based grid for easy access
-        Especially useful for those in smaller shops or those that have no third party add-in/catalog in their ConfigMgr/WSUS systems. 
+	You can configure all aspects of the email (SMTP server, recipients, body, etc.)
+	The body is designed to be an HTML-based grid for easy access
+	Especially useful for those in smaller shops or those that have no third party add-in/catalog in their ConfigMgr/WSUS systems. 
 #>
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
