c931d1d29c60ba1f993a8817fc21edab4fc08bfc
Jay Harper <jayharper@gmail.com>
2019-03-27 12:33:27 -0400

    Create Get-OnlineVerAdobeReader.ps1

---
+++ Get-OnlineVerAdobeReader.ps1
@@ -0,0 +1,135 @@
+<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	PowerShell ISE (Win10 17763)
+	 Revision:		v5
+	 Last Modified: 27 Mar 2019
+	 Created by:   	Jay Harper (github.com/thecatdidit/powershellusefulscripts)
+	 Organizaiton: 	Happy Days Are Here Again
+	 Filename:     	Get-OnlineVerAdobeReader.ps1
+	===========================================================================
+	.CHANGELOG
+	
+	[2019.27.03.1230]
+	Script creation
+	.SYNOPSIS
+        Queries the Adobe Website for the current version of
+        Adobe Acrobat Reader DC. The script returns the version, date updated, and
+        download URLs if available.
+	.DESCRIPTION
+        This function retrieves the latest data associated with Adobe Reader.
+        Utilizes Invoke-WebRequest to query Adobe Reader's release notes pagean
+        and pulls out the Version, Update Date and Download URLs for
+        the app (x64 only) It then outputs the information as a
+        PSObject to the Host.
+	.EXAMPLE
+        PS C:\> Get-OnlineVerAdobeReader.ps1
+
+        Software_Name    : Adobe Acrobat Reader DC
+        Software_URL     : https://helpx.adobe.com/acrobat/release-note/release-notes-acrobat-reader.html
+        Online_Version   : 19.010.20098
+        Online_Date      : Feb 21,2019
+        Download_URL_x64 : http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/1901020098/AcrodrDCUpd1901020098.msp
+
+        PS C:\> Get-OnlineVerAdobeReader.ps1 -Quiet
+        19.010.20098
+
+	.INPUTS
+        -Quiet
+            Use of this parameter will output just the current version of
+            Google Chrome instead of the entire object. It will always be the
+            last parameter.
+	.OUTPUTS
+        An object containing the following:
+        Software Name: Name of the software
+        Software URL: The URL info was sourced from
+        Online Version: The current version found
+        Online Date: The date the version was updated
+        Download URL x86: Download URL for the win32 version
+        Download URL x64: Download URL for the win64 version
+    
+        If -Quiet is specified then just the value of 'Online Version'
+        will be displayed.
+	.NOTES
+        Resources/Credits:
+        https://github.com/itsontheb
+
+#>
+
+function Get-OnlineVerAdobeReader
+{
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory=$false, 
+                   Position=0)]
+        [switch]
+        $Quiet
+    )
+
+    begin
+    {
+        # Initial Variables
+        $softwareName = 'Adobe Acrobat Reader DC'
+        $uri = 'https://helpx.adobe.com/acrobat/release-note/release-notes-acrobat-reader.html'
+            
+        $hashtable = [ordered]@{
+            'Software_Name'    = $softwareName
+            'Software_URL'     = $uri
+            'Online_Version'   = 'UNKNOWN' 
+            'Online_Date'      = 'UNKNOWN'
+            'Download_URL_x64' = 'UNKNOWN'
+    }
+    
+        $swObject = New-Object -TypeName PSObject -Property $hashtable
+}
+   Process
+    {
+        # Get the Version & Release Date
+        try
+        {
+        $VersionRegex = "\d+(\.\d+)+"
+        $html = Invoke-WebRequest -UseBasicParsing -Uri "$uri"
+        $DC_Versions = $html.Links | Where-Object outerHTML -Match "\($VersionRegex\)"
+        $versionArray = @()
+        foreach ($version in $DC_Versions) {
+        $VersionNumber = [regex]::match($Version.outerHTML ,"$VersionRegex").Value
+        $versionArray += $VersionNumber
+        }
+        $adobeVersion = $versionArray[0]
+        
+        $site = (curl -Uri $uri| Select-Object -ExpandProperty Content)
+        $site -match "<td valign=""top""><p><strong>Focus</strong></p>`n</td>`n</tr><tr><td>(?<content>.*)</td>" | Out-Null
+        $adobeDate = ($matches['content'])
+      
+        $urlData = $adobeVersion.Replace(".","")
+        $downloadURL = 'http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/' + $urlData + "/AcrodrDCUpd" + $urlData + ".msp"
+        
+        $swObject.Download_URL_x64 = $downloadURL
+        $swObject.Online_Version = $adobeVersion
+        $swObject.Online_Date = $adobeDate
+        
+        }
+        catch
+        {
+            Write-Verbose -Message "Error accessing the below URL: `n $URI"
+            $message = $("Line {0} : {1}" -f $_.InvocationInfo.ScriptLineNumber, $_.exception.message)
+            $swObject | Add-Member -MemberType NoteProperty -Name 'ERROR' -Value $message
+        }
+        finally
+        {
+      }
+    }
+    End
+    {
+        # Output to Host
+        if ($Quiet)
+        {
+            Write-Verbose -Message '$Quiet was specified. Returning just the version'
+            Return $swObject.Online_Version
+        }
+        else
+        {
+            Return $swobject
+        }
+    }
+}  # END Function Get-OnlineAdobeReader
