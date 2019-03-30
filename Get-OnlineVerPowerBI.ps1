9fca2b5dfce9085f06ea169d2189fc8ab2ed7e8e
Jay Harper <jayharper@gmail.com>
2019-03-30 14:12:25 -0400

    Create Get-OnlineVerPowerBI.ps1

---
+++ Get-OnlineVerPowerBI.ps1
@@ -0,0 +1,128 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	PowerShell ISE (Win10 17134)
+	 Revision:		v1
+	 Last Modified: 30 March 2019
+	 Created by:   	Jay Harper (github.com/thecatdidit/powershellusefulscripts)
+	 Organizaiton: 	Happy Days Are Here Again
+	 Filename:     	Get-OnlineVerPowerBI.ps1
+	===========================================================================
+	.Synopsis
+        Queries the PowerBI Website for the current version of
+        the app and returns the version, date updated, and
+        download URLs if available.
+    .DESCRIPTION
+	    This function retrieves the latest data associated with PowerBI
+        Utilizes Invoke-WebRequest to query the Postman download page and
+        pulls out the Version, Update Date and Download URLs for
+        the app (x64 only) It then outputs the information as a
+        PSObject to the Host.
+    .EXAMPLE
+        PS C:\> Get-OnlineVerPowerBI
+        
+        Software_Name    : PowerBI
+        Software_URL     : https://docs.microsoft.com/en-us/power-bi/desktop-latest-update
+        Online_Version   : 2.67.5404.981
+        Online_Date      : 3/28/2019
+        Download_URL_x64 : https://download.microsoft.com/download/9/B/A/9BAEFFEF-1A68-4102-8CDF-5D28BFFE6A61/PBIDesktop_x64.msi
+
+        
+    .INPUTS
+        -Quiet
+            Use of this parameter will output just the current version of
+            Google Chrome instead of the entire object. It will always be the
+            last parameter.
+
+        PS C:\> Get-OnlineVerPowerBI -Quiet
+        2.11.593m
+
+.OUTPUTS
+        An object containing the following:
+        Software Name: Name of the software
+        Software URL: The URL info was sourced from
+        Online Version: The current version found
+        Online Date: The date the version was updated
+        Download URL x64: Download URL for the win64 version
+    
+        If -Quiet is specified then just the value of 'Online Version'
+        will be displayed.
+.NOTES
+    Resources/Credits:
+    https://github.com/itsontheb
+
+#>
+
+
+function Get-OnlineVerPowerBI
+{
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory=$false, 
+                   Position=0)]
+        [switch]
+        $Quiet
+    )
+
+    begin
+    {
+        # Initial Variables
+        $SoftwareName = 'PowerBI'
+        $uri = "https://docs.microsoft.com/en-us/power-bi/desktop-latest-update"
+            
+        $hashtable = [ordered]@{
+            'Software_Name'    = $softwareName
+            'Software_URL'     = $uri
+            'Online_Version'   = 'UNKNOWN' 
+            'Online_Date'      = 'UNKNOWN'
+            'Download_URL_x64' = 'UNKNOWN'
+        }
+    
+        $swObject = New-Object -TypeName PSObject -Property $hashtable
+}
+
+
+   Process
+    {
+        # Get the Version & Release Date
+        try
+        {
+  
+        $uri2 = 'https://www.microsoft.com/en-us/download/details.aspx?id=45331'
+        $site = (curl -uri $uri2 -UseBasicParsing | select -ExpandProperty Content)
+        $site -match "Version:                                            </div><p>(?<version>.*)</p>" | Out-Null
+        $biVersion = $matches['version']
+        $site -match "Date Published:                                            </div><p>(?<date>.*)</p>" | Out-Null
+        $biDate = $matches['date']
+        $biDownloadURL = "https://download.microsoft.com/download/9/B/A/9BAEFFEF-1A68-4102-8CDF-5D28BFFE6A61/PBIDesktop_x64.msi"
+        
+        $swObject.Online_Version = $biVersion
+        $swObject.Online_Date = $biDate
+        $swObject.Download_URL_x64 = $biDownloadURL
+ 
+         }
+        catch
+        {
+            Write-Verbose -Message "Error accessing the below URL: `n $URI"
+            $message = $("Line {0} : {1}" -f $_.InvocationInfo.ScriptLineNumber, $_.exception.message)
+            $swObject | Add-Member -MemberType NoteProperty -Name 'ERROR' -Value $message
+        }
+        finally
+        {
+   
+    }
+    }
+    End
+    {
+        # Output to Host
+        if ($Quiet)
+        {
+            Write-Verbose -Message '$Quiet was specified. Returning just the version'
+            Return $swObject.Online_Version
+        }
+        else
+        {
+            Return $swobject
+        }
+    }
+}  # END Function Get-OnlineVerPowerBI
