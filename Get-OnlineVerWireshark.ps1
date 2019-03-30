2dcb161d854a3a035b38d0124233d1f2eb57d35b
Jay Harper <jayharper@gmail.com>
2019-03-30 13:19:35 -0400

    Cleanup and new Submissions

--- Get-OnlineVerWireshark.ps1
+++ Get-OnlineVerWireshark.ps1
@@ -6,36 +6,40 @@
 	 Last Modified: 30 March 2019
 	 Created by:   	Jay Harper (github.com/thecatdidit/powershellusefulscripts)
 	 Organizaiton: 	Happy Days Are Here Again
-	 Filename:     	Get-OnlineVerWinSCP.ps1
+	 Filename:     	Get-OnlineVerWireshark
 	===========================================================================
 	.Synopsis
-        Queries the WinSCP Website for the current version of
+        Queries the Wireshark Website for the current version of
         the app and returns the version, date updated, and
         download URLs if available.
+
+        Wireshark maintains a PAD file for automation systems to keep 
+        track of new releases.
+
+        Site: https://www.wireshark.org/wireshark-pad.xml
     .DESCRIPTION
-	    This function retrieves the latest data associated with WinSCP
-        Utilizes Invoke-WebRequest to query the Postman download page and
+	    This function retrieves the latest data associated with Wireshark
+        Utilizes Invoke-WebRequest to query the Wireshark download page and
         pulls out the Version, Update Date and Download URLs for
         the app (x64 only) It then outputs the information as a
         PSObject to the Host.
     .EXAMPLE
-        PS C:\> Get-OnlineVerWinSCP
+        PS C:\> Get-OnlineVerWireshark
         
-        Software_Name    : WinSCP
-        Software_URL     : https://winscp.net/eng/news.php
-        Online_Version   : 5.15
-        Online_Date      : 2019-03-27
-        Download_URL_x64 : https://winscp.net/download/WinSCP-5.15-Setup.exe
+       Software_Name    : Wireshark
+       Software_URL     : https://support.bluejeans.com/knowledge/desktop-app-deployment
+       Online_Version   : 3.0.0
+       Online_Date      : 2019-02-28
+       Download_URL_x64 : https://1.na.dl.wireshark.org/win64/Wireshark-win64-3.0.0.exe
 
-        
     .INPUTS
         -Quiet
             Use of this parameter will output just the current version of
             Google Chrome instead of the entire object. It will always be the
             last parameter.
 
-        PS C:\> Get-OnlineVerBlueJeans -Quiet
-        5.15
+        PS C:\> Get-OnlineVerWireshark -Quiet
+        3.0.0
 
 .OUTPUTS
         An object containing the following:
@@ -54,7 +58,7 @@
 #>
 
 
-function Get-OnlineVerWinSCP
+function Get-OnlineVerWireshark
 {
     [cmdletbinding()]
     param (
@@ -67,8 +71,8 @@ function Get-OnlineVerWinSCP
     begin
     {
         # Initial Variables
-        $SoftwareName = 'WinSCP'
-        $uri = "https://winscp.net/eng/news.php"
+        $SoftwareName = 'Wireshark'
+        $uri = "https://support.bluejeans.com/knowledge/desktop-app-deployment"
             
         $hashtable = [ordered]@{
             'Software_Name'    = $softwareName
@@ -88,19 +92,16 @@ function Get-OnlineVerWinSCP
         try
         {
   
-        $site = (Invoke-WebRequest -uri $uri -UseBasicParsing).Content
-
-        $site -match "<p class=""items-list-blocks-item-date""><span class=""sr-only"">Published:</span>(?<date>.*)</p>" | Out-Null
-        $winscpdate = $matches['date']
-
-        $site -match "<h2 class=""items-list-blocks-item-heading"">WinSCP (?<version>.*) released</h2>" | Out-Null
-        $winscpVersion = $matches['version']
-
-        $winscpURL = "https://winscp.net/download/WinSCP-" + $winscpVersion + "-Setup.exe"
+        $uri = 'https://www.wireshark.org/wireshark-pad.xml'
+        $xml_versions = New-Object XML
+        $xml_versions.Load($uri)
+        $wiresharkVersion = ($xml_versions.XML_DIZ_INFO.Program_Info.Program_Version).ToString()
+        $wiresharkDate = ($xml_versions.XML_DIZ_INFO.Program_Info.Program_Release_Year + "-" + $xml_versions.XML_DIZ_INFO.Program_Info.Program_Release_Month + "-" + $xml_versions.XML_DIZ_INFO.Program_Info.Program_Release_Day).ToString()
+        $wiresharkURL = ("https://1.na.dl.wireshark.org/win64/Wireshark-win64-" + $wiresharkVersion + ".exe").ToString()
         
-        $swObject.Online_Version = $winscpVersion
-        $swObject.Online_Date = $winscpdate
-        $swObject.Download_URL_x64 = $winscpURL
+        $swObject.Online_Version = $wiresharkVersion
+        $swObject.Online_Date = $wiresharkDate
+        $swObject.Download_URL_x64 = $wiresharkURL
  
          }
         catch
@@ -127,4 +128,24 @@ function Get-OnlineVerWinSCP
             Return $swobject
         }
     }
-}  # END Function Get-OnlineVerWinSCP
+}  # END Function Get-OnlineVerBlueJeans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$uri = 'https://www.wireshark.org/wireshark-pad.xml'
+$xml_versions.Load($uri)
+$wiresharkVersion = $xml_versions.XML_DIZ_INFO.Program_Info.Program_Version
+$wiresharkDate = $xml_versions.XML_DIZ_INFO.Program_Info.Program_Release_Year + "-" + $xml_versions.XML_DIZ_INFO.Program_Info.Program_Release_Month + "-" + $xml_versions.XML_DIZ_INFO.Program_Info.Program_Release_Day
+$wiresharkURL = "https://1.na.dl.wireshark.org/win64/Wireshark-win64-" + $wiresharkVersion + ".exe"
