0baa6a9aca680cc884c0c66946791a45c1768c75
Jay Harper <jayharper@gmail.com>
2021-07-14 16:53:24 -0400

    Create Get-3PUPChecks.ps1

---
+++ _Skunkworks/Get-3PUPChecks.ps1
@@ -0,0 +1,4 @@
+iex ((New-Object System.Net.WebClient).DownloadString(' https://raw.githubusercontent.com/thecatdidit/PowerShellUsefulScripts/master/Get-OnlineVerPuTTY.ps1 '))
+iex ((New-Object System.Net.WebClient).DownloadString(' https://raw.githubusercontent.com/thecatdidit/PowerShellUsefulScripts/master/Get-OnlineVerAcrobatReader.ps1 '))
+iex ((New-Object System.Net.WebClient).DownloadString(' https://raw.githubusercontent.com/thecatdidit/PowerShellUsefulScripts/master/Get-OnlineVerAudacity.ps1 '))
+iex ((New-Object System.Net.WebClient).DownloadString(' https://raw.githubusercontent.com/thecatdidit/PowerShellUsefulScripts/master/Get-OnlineVerFirefox.ps1 '))
