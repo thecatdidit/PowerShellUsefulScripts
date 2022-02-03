5a7fd69eaf3f362d6f6656831bcaed1c8e4be19a
Jay Harper <jayharper@gmail.com>
2022-02-03 09:44:09 -0500

    Update Set-Intel5GHz.ps1

--- Set-Intel5GHz.ps1
+++ Set-Intel5GHz.ps1
@@ -1,4 +1,6 @@
-<# This script searches for Intel Wifi adapters on a system, and it sets the preferred wifi band to 5GHz. I will get this formalized in my template soon -JH 020322 #>
+<# This script searches for Intel Wifi adapters on a system, and it sets the preferred wifi band to 5GHz. I will get this formalized in my template soon -JH 020322 
+   Full credit for this script goes to https://www.theictguy.co.uk/set-intel-wi-fi-preferred-band-via-script/
+#>
 
 Get-ChildItem HKLM:\SYSTEM\CurrentControlSet\Control\Class -Recurse -ErrorAction silentlyContinue | Get-ItemProperty | ForEach-Object {if  ($_.RoamingPreferredBandType -ge 0) {
     $path = $_.pspath
