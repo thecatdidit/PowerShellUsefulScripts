cf264e24b9892a697d89358bf0196815b2a68881
Jay Harper <jayharper@gmail.com>
2022-05-24 10:21:59 -0400

    Modified parsing for updated host content

--- Get-OnlineVerMattermost.ps1
+++ Get-OnlineVerMattermost.ps1
@@ -2,15 +2,17 @@
     ===========================================================================
      Created with:  PowerShell ISE - Win10 21H1/19043
      Revision:      v1
-     Last Modified: 06 Jan 2022
+     Last Modified: 24 May 2022
      Created by:    Jay Harper (github.com/thecatdidit/powershellusefulscripts)
      Organizaiton:  Happy Days Are Here Again
      Filename:      Get-OnlineVerMattermost.ps1
     ===========================================================================
     .CHANGELOG
-    [v1 - 2021.06.22]
-    Script creatiom
-
+    [2022.05.23]
+    Updated parsing logic to scrape updated host content
+    [2022.01.06]
+    Script creation
+    
     .SYNOPSIS
         Queries the MatterMost webside for the current version of
         the app and returns the version, date updated, and
@@ -62,12 +64,14 @@
             https://github.com/itsontheb
 #>
 
-$SearchString = "<li><p><strong>v(?<version>.*), released (?<date>.*)</strong></p></li>"
+$MMVersionSearchString = "<h2>Release v(?<version>.*)<a class=""headerlink"" href=""#id1"" title=""Permalink to this headline"">"
+$MMDateSearchString = "<p><strong>Release day: (?<date>.*)</strong></p>"
 $MMWebsite = (Invoke-WebRequest -Uri 'https://docs.mattermost.com/install/desktop-app-changelog.html' -UseBasicParsing | Select-Object -ExpandProperty Content)
-$MMWebsite -match $SearchString | Out-Null
-$ReleaseVersion = ($Matches['version'])
-$ReleaseDate = ($Matches['date'])
 
+$MMWebsite -match $MMVersionSearchString | Out-Null
+$ReleaseVersion = ($Matches['version'])
+$MMWebsite -match $MMDateSearchString | Out-Null
+$ReleaseDate = ($Matches['date'])
 
 $STDRelease = "https://releases.mattermost.com/desktop/" + $ReleaseVersion + "/mattermost-desktop-setup-" + $ReleaseVersion + "-win.exe?src=dl"
 $MSIRelease = "https://releases.mattermost.com/desktop/" + $ReleaseVersion + "/mattermost-desktop-" + $ReleaseVersion + "-x64.msi?src=dl"
@@ -110,8 +114,8 @@ function Get-OnlineVerMattermost {
             $MMVersion = ($matches['version'])
             $MMDate = ($matches['date'])
             
-            $swObject.Online_Version = $MMVersion
-            $swObject.Online_Date = $MMDate
+            $swObject.Online_Version = $ReleaseVersion
+            $swObject.Online_Date = $ReleaseDate
 
         }
         catch {
