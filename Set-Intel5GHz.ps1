c71b67c1883779a1b0f1fef855454998b4da99ac
Non <jayharper@gmail.com>
2025-12-21 14:07:43 -0500

    Update Set-Intel5GHz.ps1

--- Set-Intel5GHz.ps1
+++ Set-Intel5GHz.ps1
@@ -1,4 +1,4 @@
-<# This script searches for Intel Wifi adapters on a system, and it sets the preferred wifi band to 5GHz. I will get this formalized in my template soon -JH 020322 
+<# This script searches for Intel Wifi adapters on a system, and it sets the preferred wifi band to 5GHz. I will get this formalized in my template soon
    Full credit for this script goes to https://www.theictguy.co.uk/set-intel-wi-fi-preferred-band-via-script/
 #>
 
